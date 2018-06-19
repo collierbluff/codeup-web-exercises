65e662f1431762cd0efd7176bc330ddd47b7d6b3
Collier Bluff <collierbluff@Colliers-MacBook-Air.local>
2018-06-19 09:50:55 -0500

    worked on js bonuses

---
+++ bonus/bonus.js
@@ -0,0 +1,141 @@
+"use strict";
+
+(function(){
+
+
+
+    // do{
+    //     var userDay = (prompt("What's your favorite day of the week?")).toLowerCase();
+    //
+    //     if (userDay === "monday"){
+    //
+    //         alert("Cool Story Bro!")
+    //     }
+    //
+    //     else if (userDay === "tuesday"){
+    //
+    //         alert("what a Tuesdayer!")
+    //     }
+    //
+    //     else if (userDay === "wednesday"){
+    //
+    //         alert("They say it's the hump day.")
+    //     }
+    //
+    //     else if (userDay === "thursday"){
+    //
+    //         alert("What a Thursdayer!")
+    //     }
+    //
+    //     else if (userDay === "friday"){
+    //
+    //         alert("They do say TGIF and all that.")
+    //     }
+    //
+    //     else if (userDay === "saturday"){
+    //
+    //         alert("And what else is new?")
+    //     }
+    //
+    //     else if (userDay === "sunday"){
+    //
+    //         alert("Take me to church?")
+    //     }
+    //
+    //     else {userDay = "invalid";}
+    //
+    // }while (userDay === "invalid");
+
+
+
+
+    // do {
+    //     var userDay = (prompt("What's your favorite day of the week?")).toLowerCase();
+    //
+    //     switch (userDay) {
+    //         case "monday":
+    //             alert("Monday!");
+    //             break;
+    //
+    //         case "tuesday":
+    //             alert("Tuesday!");
+    //             break;
+    //
+    //         case "wednesday":
+    //             alert("Wednesday!");
+    //             break;
+    //
+    //         case "thursday":
+    //             alert("Thursday!");
+    //             break;
+    //
+    //         case "friday":
+    //             alert("Friday!");
+    //             break;
+    //
+    //         case "saturday":
+    //             alert("Saturday!");
+    //             break;
+    //
+    //         case "sunday":
+    //             alert("Sunday!");
+    //             break;
+    //
+    //         default:
+    //             userDay = "invalid";
+    //             alert("Invalid Entry!");
+    //             break;
+    //     }
+    // } while (userDay === "invalid");
+
+    // function isNumeric(input) {
+    //
+    //     return !isNaN(parseFloat(input)) && isFinite(input);
+    // }
+
+    // function numberTest() {
+    //
+    //     var num = prompt("Name a number!?!?!?");
+    //
+    //     if (!isNumeric(num)){alert("That's not a number!")}
+    //     else {alert("That's a number!")}
+    // }
+
+    // function numberTest(){
+    //
+    //     var num = prompt("Name a number!?!?!?");
+    //
+    //     alert(isNumeric(num) ? "That's a number!" : "That's not a number!")
+    //
+    // }
+    //
+    // numberTest();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
