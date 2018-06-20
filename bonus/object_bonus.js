b22290dd0a1908c846d5dc9c8db718a198fb6a79
Collier Bluff <collierbluff@Colliers-MacBook-Air.local>
2018-06-20 15:51:33 -0500

    Added to bonus exercises

---
+++ bonus/object_bonus.js
@@ -0,0 +1,36 @@
+
+
+var dog = {
+
+  breed: "Mutt",
+  weightInPounds: 50,
+  age: 3,
+  color: "Darkness",
+  sterilized: true,
+  shotRecords: [
+
+      {
+          typeOfShot: "Plague",
+          date: 2018
+      },
+      {
+          typeOfShot: "Cholera",
+          date: 2017
+      },
+      {
+          typeOfShot: "Rabies",
+          date: 2016
+      }
+  ],
+    bark: function(){console.log("Woof!");},
+    getOlder: function(){this.age += 1;},
+    fix: function () {
+        if(this.sterilized === false){
+            this.sterilized = true;
+        }
+    },
+    vaccinate: function (name, year) {
+        this.shotRecords.push({typeOfShot: name, date: year});
+    }
+
+};
